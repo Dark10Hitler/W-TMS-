499b9a9bf7857b86b2a6c2f0127644db01a4df42
Dark10Hitler <157166160+Dark10Hitler@users.noreply.github.com>
2026-03-24 20:36:41 +0200

    Update config_topology.py

--- config_topology.py
+++ config_topology.py
@@ -3,8 +3,8 @@ import string
 from supabase import create_client, Client
 
 # --- НАСТРОЙКИ ПОДКЛЮЧЕНИЯ ---
-url = "YOUR_SUPABASE_URL"
-key = "YOUR_SUPABASE_KEY"
+url = "https://grdyokwemanzcpmfvhps.supabase.co"
+key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdyZHlva3dlbWFuemNwbWZ2aHBzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzM0OTg0NTMsImV4cCI6MjA4OTA3NDQ1M30.CL_UETOVA-Fw1b4vJc_rQUUiuE44cWD2qDYWPog4S0w"
 supabase: Client = create_client(url, key)
 
 class WarehouseManager:
